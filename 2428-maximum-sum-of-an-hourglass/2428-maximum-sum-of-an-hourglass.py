1539c04b93b2a06c4cda78bfd7c5f005c9a9fcd0
dyuthimalepati9 <dyuthimalepati9@gmail.com>
2026-05-01 13:53:47 +0530

    Time: 26 ms (89.66%), Space: 21.9 MB (79.31%) - LeetHub

---
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSum(self, grid: List[List[int]]) -> int:
+
+        m, n, ans = len(grid)-1, len(grid[0])-1, 0
+
+        for i, j in product(range(1, m), range(1,n)):
+
+                sm = ( grid[i-1][j-1] + grid[i-1][j] + grid[i-1][j+1]
+                                      + grid[i  ][j] + # <-- hourglass center
+                       grid[i+1][j-1] + grid[i+1][j] + grid[i+1][j+1] )
+
+                if sm > ans: ans = sm
+
+        return ans 
